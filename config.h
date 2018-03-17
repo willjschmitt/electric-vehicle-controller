6aa4fb87d472ea4b1d8ca3c8faae689d4cd233e6
William Schmitt <william@wschmitt.com>
2018-03-17 16:52:10 -0700

    Simplifying configuration.

--- config.h
+++ config.h
@@ -5,63 +5,24 @@
 
 // 'C' source line config statements
 
-// DEVCFG3
-// USERID = No Setting
-#pragma config FUSBIDIO2 = ON           // USB2 USBID Selection (USBID pin is controlled by the USB2 module)
-#pragma config FVBUSIO2 = ON            // USB2 VBUSON Selection bit (VBUSON pin is controlled by the USB2 module)
-#pragma config PGL1WAY = ON             // Permission Group Lock One Way Configuration bit (Allow only one reconfiguration)
-#pragma config PMDL1WAY = ON            // Peripheral Module Disable Configuration (Allow only one reconfiguration)
-#pragma config IOL1WAY = ON             // Peripheral Pin Select Configuration (Allow only one reconfiguration)
-#pragma config FUSBIDIO1 = ON           // USB1 USBID Selection (USBID pin is controlled by the USB1 module)
-#pragma config FVBUSIO1 = ON            // USB2 VBUSON Selection bit (VBUSON pin is controlled by the USB1 module)
-
-// DEVCFG2
+// System frequency:
+// SPLL => 4MHz
+// f = 4MHz / FPLLIDIV * FPLLMULT / FPLLODIV
+//   = 4MHz / 1 * 30 / 2
+//   = 4MHz * 25
+//   = 100MHz
+#pragma config FNOSC = SPLL             // Oscillator Selection Bits (System PLL)
 #pragma config FPLLIDIV = DIV_1         // System PLL Input Divider (1x Divider)
 #pragma config FPLLRNG = RANGE_5_10_MHZ // System PLL Input Range (5-10 MHz Input)
 #pragma config FPLLICLK = PLL_FRC       // System PLL Input Clock Selection (FRC is input to the System PLL)
 #pragma config FPLLMULT = MUL_50        // System PLL Multiplier (PLL Multiply by 50)
 #pragma config FPLLODIV = DIV_2         // System PLL Output Clock Divider (2x Divider)
-#pragma config VBATBOREN = ON           // VBAT BOR Enable (Enable ZPBOR during VBAT Mode)
-#pragma config DSBOREN = OFF            // Deep Sleep BOR Enable (Disable ZPBOR during Deep Sleep Mode)
-#pragma config DSWDTPS = DSPS32         // Deep Sleep Watchdog Timer Postscaler (1:2^36)
-#pragma config DSWDTOSC = LPRC          // Deep Sleep WDT Reference Clock Selection (Select LPRC as DSWDT Reference clock)
-#pragma config DSWDTEN = ON             // Deep Sleep Watchdog Timer Enable (Enable DSWDT during Deep Sleep Mode)
-#pragma config FDSEN = ON               // Deep Sleep Enable (Enable DSEN bit in DSCON)
-#pragma config BORSEL = HIGH            // Brown-out trip voltage (BOR trip voltage 2.1v (Non-OPAMP deviced operation))
-#pragma config UPLLEN = OFF             // USB PLL Enable (USB PLL Disabled)
 
-// DEVCFG1
-#pragma config FNOSC = SPLL             // Oscillator Selection Bits (System PLL)
-#pragma config DMTINTV = WIN_127_128    // DMT Count Window Interval (Window/Interval value is 127/128 counter value)
-#pragma config FSOSCEN = ON             // Secondary Oscillator Enable (Enable Secondary Oscillator)
-#pragma config IESO = ON                // Internal/External Switch Over (Enabled)
-#pragma config POSCMOD = OFF            // Primary Oscillator Configuration (Primary osc disabled)
-#pragma config OSCIOFNC = OFF           // CLKO Output Signal Active on the OSCO Pin (Disabled)
-#pragma config FCKSM = CSECME           // Clock Switching and Monitor Selection (Clock Switch Enabled, FSCM Enabled)
-#pragma config WDTPS = PS1048576        // Watchdog Timer Postscaler (1:1048576)
-#pragma config WDTSPGM = STOP           // Watchdog Timer Stop During Flash Programming (WDT stops during Flash programming)
-#pragma config WINDIS = NORMAL          // Watchdog Timer Window Mode (Watchdog Timer is in non-Window mode)
+// Turn off timers and features not used.
 #pragma config FWDTEN = OFF             // Watchdog Timer Enable (WDT Enabled)
-#pragma config FWDTWINSZ = WINSZ_25     // Watchdog Timer Window Size (Window size is 25%)
-#pragma config DMTCNT = DMT31           // Deadman Timer Count Selection (2^31 (2147483648))
 #pragma config FDMTEN = OFF             // Deadman Timer Enable (Deadman Timer is enabled)
-
-// DEVCFG0
 #pragma config DEBUG = OFF              // Background Debugger Enable (Debugger is disabled)
 #pragma config JTAGEN = OFF             // JTAG Enable (JTAG Port Enabled)
-#pragma config ICESEL = ICS_PGx1        // ICE/ICD Comm Channel Select (Communicate on PGEC1/PGED1)
-#pragma config TRCEN = ON               // Trace Enable (Trace features in the CPU are enabled)
-#pragma config BOOTISA = MIPS32         // Boot ISA Selection (Boot code and Exception code is MIPS32)
-#pragma config FSLEEP = OFF             // Flash Sleep Mode (Flash is powered down when the device is in Sleep mode)
-#pragma config DBGPER = PG_ALL          // Debug Mode CPU Access Permission (Allow CPU access to all permission regions)
-#pragma config SMCLR = MCLR_NORM        // Soft Master Clear Enable (MCLR pin generates a normal system Reset)
-#pragma config SOSCGAIN = GAIN_2X       // Secondary Oscillator Gain Control bits (2x gain setting)
-#pragma config SOSCBOOST = ON           // Secondary Oscillator Boost Kick Start Enable bit (Boost the kick start of the oscillator)
-#pragma config POSCGAIN = GAIN_LEVEL_3  // Primary Oscillator Gain Control bits (Gain Level 3 (highest))
-#pragma config POSCBOOST = ON           // Primary Oscillator Boost Kick Start Enable bit (Boost the kick start of the oscillator)
-#pragma config EJTAGBEN = NORMAL        // EJTAG Boot Enable (Normal EJTAG functionality)
-
-// DEVCP
 #pragma config CP = OFF                 // Code Protect (Protection Disabled)
 
 // SEQ
